989cfdfc18f9e5151e13937f7f3f94df410fc1ae
chelle02 <ewan.d.chelley@bbc.co.uk>
2024-12-23 16:41:50 +0000

    Initial Commit

---
+++ MyBrightcovePlayerUIKitTests/MyBrightcovePlayerUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBrightcovePlayerUIKitTests.swift
+//  MyBrightcovePlayerUIKitTests
+//
+//  Created by Ewan Chelley on 23/12/2024.
+//
+
+import Testing
+@testable import MyBrightcovePlayerUIKit
+
+struct MyBrightcovePlayerUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
